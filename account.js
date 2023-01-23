3a9bc9c724771b6e0def93f6081323411a09ad72
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-23 11:33:16 +0000

    passed green on initial feature test

---
+++ account.js
@@ -0,0 +1,6 @@
+class Account {
+  printStatement() {
+    return "date || credit || debit || balance";
+  }
+}
+module.exports = Account;
--- bankFeature.test.js
+++ bankFeature.test.js
@@ -1,35 +1,29 @@
+const Account = require("./account");
+
 describe("returns printed bank statement", () => {
   it("when no transaction haves been made", () => {
     const account = new Account();
-    expect(account.printStatement().tobe("date || credit || debit || balance"));
+    expect(account.printStatement()).toBe("date || credit || debit || balance");
   });
-  it("when a deposit of 1000 on 10-01-2023", () => {
+  xit("when a deposit of 1000 on 10-01-2023", () => {
     const account = new Account();
     const deposit = new Transaction("credit", 1000, "10-01-2023");
     account.add(deposit);
-    expect(
-      account
-        .printStatement()
-        .tobe(
-          "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
-        )
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
     );
   });
-  it("when a deposit of 1000 on 10-01-2023 and a deposit of 2000 on 13-01-2023", () => {
+  xit("when a deposit of 1000 on 10-01-2023 and a deposit of 2000 on 13-01-2023", () => {
     const account = new Account();
     const depositOne = new Transaction("credit", 1000, "10-01-2023");
     account.add(depositOne);
     const depositTwo = new Transaction("credit", 2000, "13-01-2023");
     account.add(depositTwo);
-    expect(
-      account
-        .printStatement()
-        .tobe(
-          "date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
-        )
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
     );
   });
-  it("when a deposit of 1000 on 10-01-2023, a deposit of 2000 on 13-01-2023 and a withdrawal of 500 on 14-01-2023", () => {
+  xit("when a deposit of 1000 on 10-01-2023, a deposit of 2000 on 13-01-2023 and a withdrawal of 500 on 14-01-2023", () => {
     const account = new Account();
     const depositOne = new Transaction("credit", 1000, "10-01-2023");
     account.add(depositOne);
@@ -37,12 +31,8 @@ describe("returns printed bank statement", () => {
     account.add(depositTwo);
     const withdrawal = new Transaction("debit", 500, "14-01-2023");
     account.add(withdrawal);
-    expect(
-      account
-        .printStatement()
-        .tobe(
-          "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
-        )
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n14/01/2023 || || 500.00 || 2500.00\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
     );
   });
 });
