ce2bf2cc4f8a41f21e531a1f91eb07ecee8d93df
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-23 17:41:46 +0000

    passed account class unit test using mocking

---
+++ accountUnit.test.js
@@ -0,0 +1,19 @@
+const Account = require("./account");
+
+describe("returns printed bank statement", () => {
+  it("when no transaction have been made", () => {
+    const account = new Account();
+    expect(account.printStatement()).toBe("date || credit || debit || balance");
+  });
+  it("when a deposit of 1000 on 10/01/2023", () => {
+    const account = new Account();
+    const depositDouble = {
+      getValue: () => 1000,
+      getStatmentFormat: () => "\n10/01/2023 || 1000.00 || ||",
+    };
+    account.add(depositDouble);
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
+});
