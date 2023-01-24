ad72edeef0d23937997c807c7e114e5787acf5bf
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2023-01-24 15:00:14 +0000

    passed account unit test deposit and withdrawal using mocked values

--- accountUnit.test.js
+++ accountUnit.test.js
@@ -30,4 +30,62 @@ describe("adds transactions returns printed bank statement", () => {
       "date || credit || debit || balance\n10/01/2023 || 1000.00 || || 1000.00"
     );
   });
+  it("when a deposit of 1000 on 10/01/2023 and a withdrawal of 500.50 on 14/01/2023", () => {
+    const account = new Account();
+
+    const depositDouble = {
+      getValue: () => 1000,
+      getStatmentFormat: () => "\n10/01/2023 || 1000.00 || ||",
+      validType: () => true,
+      validAmount: () => true,
+      validDate: () => true,
+      date: "10/01/2023",
+    };
+    account.add(depositDouble);
+
+    const withdrawalDouble = {
+      getValue: () => -500.5,
+      getStatmentFormat: () => "\n14/01/2023 || || 500.50 ||",
+      validType: () => true,
+      validAmount: () => true,
+      validDate: () => true,
+      date: "14/01/2023",
+    };
+    account.add(withdrawalDouble);
+
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n14/01/2023 || || 500.50 || 499.50\n10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
+  xit("when two transactions not in date order", () => {
+    const account = new Account();
+
+    const depositOneDouble = {
+      getValue: () => 2000,
+      getStatmentFormat: () => "\n13/01/2023 || 2000.00 || ||",
+      validType: () => true,
+      validAmount: () => true,
+      validDate: () => true,
+      date: "13/01/2023",
+    };
+    account.add(depositOneDouble);
+
+    const depositTwoDouble = {
+      getValue: () => 1000,
+      getStatmentFormat: () => "\n10/01/2023 || 1000.00 || ||",
+      validType: () => true,
+      validAmount: () => true,
+      validDate: () => true,
+      date: "10/01/2023",
+    };
+    account.add(depositTwoDouble);
+
+    expect(account.sortTransactionsByDate()).toEqual([
+      { amount: 1000, date: "10/01/2023", type: "credit" },
+      { amount: 2000, date: "13/01/2023", type: "credit" },
+    ]);
+    expect(account.printStatement()).toBe(
+      "date || credit || debit || balance\n13/01/2023 || 2000.00 || || 3000.00\n10/01/2023 || 1000.00 || || 1000.00"
+    );
+  });
 });
